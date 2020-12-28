36ebf6205a3286f37f787093485381fb81a9b808
rogerwmwong <rogerwmwong@gmail.com>
2020-12-28 12:07:20 +1100

    Update Fruit1.pm

--- lib/Fruit1.pm
+++ lib/Fruit1.pm
@@ -43,4 +43,11 @@ use warnings;
 
 1;
 
-                                                
+__END__
+=head1 NAME
+Fruit.pm - Define Fruit Object - Getters and Setters
+=head1 SYNOPSIS
+Fruit has name, colour and taste
+=head1 DESCRIPTION
+Define attributes for Fruit using test driven development
+=cut                                                
